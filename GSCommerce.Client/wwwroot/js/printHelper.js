618cfde709f33bfc8f88b92334a32676374b2870
josuecharls <josuecharls1@gmail.com>
2025-07-04 23:13:18 -0500

    Configurando Guias

--- GSCommerce.Client/wwwroot/js/printHelper.js
+++ GSCommerce.Client/wwwroot/js/printHelper.js
@@ -20,3 +20,13 @@
     `);
     ventana.document.close();
 };
+
+// Imprime texto plano como ticket (tipo POS o térmica)
+window.imprimirTicketTexto = (texto) => {
+    const win = window.open("", "_blank", "width=400,height=600");
+    win.document.write(`<pre style="font-family: monospace; font-size:12px;">${texto}</pre>`);
+    win.document.close();
+    win.focus();
+    win.print();
+    win.close();
+};
