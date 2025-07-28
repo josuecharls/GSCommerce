722570cb998472ca54a9ad44acfd13b8f4a6e9b3
josuecharls <josuecharls1@gmail.com>
2025-07-28 18:57:55 -0500

    agregando detalles produccion

---
+++ GSCommerce.Client/publish/wwwroot/js/excelExport.js
@@ -0,0 +1,7 @@
+﻿window.exportToExcel = (headers, rows, filename) => {
+    const wb = XLSX.utils.book_new();
+    const wsData = [headers, ...rows];
+    const ws = XLSX.utils.aoa_to_sheet(wsData);
+    XLSX.utils.book_append_sheet(wb, ws, "Hoja1");
+    XLSX.writeFile(wb, filename + ".xlsx");
+};
