e4413d1547ce3c24bf837ab5a919fe8fa2e3bbbb
josuecharls <josuecharls1@gmail.com>
2025-08-03 13:31:01 -0500

    agregando nuevo reporte

---
+++ GSCommerce.Client/Models/DTOs/Reportes/ComparativoVendedorDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace GSCommerce.Client.Models.DTOs.Reportes
+{
+    public class ComparativoVendedorDTO
+    {
+        public string Vendedor { get; set; } = string.Empty;
+        public int VentasHoy { get; set; }
+        public int ClientesHoy { get; set; }
+        public decimal MontoHoy { get; set; }
+        public int VentasOtraFecha { get; set; }
+        public int ClientesOtraFecha { get; set; }
+        public decimal MontoOtraFecha { get; set; }
+    }
+}
